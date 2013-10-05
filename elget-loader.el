1c262acf115efedeabd952230574a247e56a4061
Mikhail Shevchuk <mico@Mikhails-MacBook-Pro.local>
2013-10-05 12:10:47 +0400

    elget-loader was added that loads needed els

---
+++ elget-loader.el
@@ -0,0 +1,33 @@
+;; ElHome
+(defconst elhome-directory "~/.emacs.d/")
+
+(add-to-list 'load-path (concat (file-name-as-directory elhome-directory) "el-get/el-get"))
+
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (let (el-get-master-branch)
+       (end-of-buffer)
+       (eval-print-last-sexp)))))
+(let ((emacswiki-recipes (concat el-get-dir "el-get/recipes/emacswiki")))
+  (unless (file-exists-p emacswiki-recipes)
+    (el-get-emacswiki-refresh emacswiki-recipes t)))
+
+(push (concat elhome-directory "/site-lisp/recipes/") el-get-recipe-path)
+
+(setq my-packages
+      (append '(expand-region
+		tern
+		js2-mode
+		dsvn
+                elhome
+                )
+              (mapcar 'el-get-source-name el-get-sources)))
+(add-hook 'el-get-post-install-hooks 'el-get-init)
+
+;; you should be connected to Net at this point
+(el-get 'sync my-packages)
+
+(require 'elhome)
+(elhome-init)
