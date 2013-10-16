3dce9cd5d743193131b891ae8ae3462f164c0d76
Mikhail Shevchuk <mikhail.shevchuk@gmail.com>
2013-10-16 14:44:43 +0400

    improving keybindings

--- init.el
+++ init.el
@@ -29,3 +29,19 @@
 (package-initialize)
 (setq ergoemacs-theme "lvl2")
 (ergoemacs-mode 1)
+
+;; Move to beginning/ending of line
+(defconst ergoemacs-move-beginning-of-line-key	(kbd "M-H"))
+(defconst ergoemacs-move-end-of-line-key	(kbd "M-L"))
+
+(define-key ergoemacs-keymap ergoemacs-move-end-of-line-key 'move-end-of-line)
+(define-key ergoemacs-keymap ergoemacs-move-beginning-of-line-key 'move-beginning-of-line)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+(when (eq system-type 'darwin) ;; mac specific settings
+  (setq mac-option-modifier 'alt)
+  (setq mac-command-modifier 'meta))
+
