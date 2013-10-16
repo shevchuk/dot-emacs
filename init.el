6375c06d0f5a4c101345b14a698fc17a725c9916
Mikhail Shevchuk <mikhail.shevchuk@gmail.com>
2013-10-16 16:03:20 +0400

    auto-complete + js2-mode hooks were added

--- init.el
+++ init.el
@@ -4,13 +4,29 @@
 ;; these packages will be automatically installed if there is an internet connection
 (defvar packages-to-load
   '(
+    auto-complete
     dsvn
     expand-region
     js2-mode
     magit
     ergoemacs-keybindings
     tern
+    projectile
 ))
+
+;; javascript
+(add-hook 'js2-mode-hook 
+	  (lambda () 
+	    (tern-mode t)
+	    (projectile-mode t)
+	    (auto-complete-mode 1)))
+
+(eval-after-load 'tern
+   '(progn
+      (require 'tern-auto-complete)
+      (tern-ac-setup)))
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 ;; install packages if they are not installed yet
 (require 'elget-loader)
@@ -37,11 +53,17 @@
 (define-key ergoemacs-keymap ergoemacs-move-end-of-line-key 'move-end-of-line)
 (define-key ergoemacs-keymap ergoemacs-move-beginning-of-line-key 'move-beginning-of-line)
 
+;; tabs
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
 
+;; keymap
 (when (eq system-type 'darwin) ;; mac specific settings
   (setq mac-option-modifier 'control)
   (setq mac-command-modifier 'meta))
+
+;; editor
+(define-key isearch-mode-map "\C-f" 'isearch-repeat-forward)
+(global-set-key (kbd "M-0") 'er/expand-region)
 
