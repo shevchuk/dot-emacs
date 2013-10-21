4840fe630a5850c66ca812893f2450a1098fb339
Mikhail Shevchuk <mikhail.shevchuk@gmail.com>
2013-10-21 23:40:39 +0400

    keymap improvements

--- init.el
+++ init.el
@@ -56,9 +56,20 @@
 ;; Move to beginning/ending of line
 (defconst ergoemacs-move-beginning-of-line-key	(kbd "M-H"))
 (defconst ergoemacs-move-end-of-line-key	(kbd "M-L"))
+(defconst ergoemacs-delete-window (kbd "M-3"))
+(defconst ergoemacs-delete-other-windows (kbd "M-4"))
+(defconst ergoemacs-split-window-below (kbd "M-8"))
+(defconst ergoemacs-split-window-right (kbd "M-9"))
+
 
 (define-key ergoemacs-keymap ergoemacs-move-end-of-line-key 'move-end-of-line)
 (define-key ergoemacs-keymap ergoemacs-move-beginning-of-line-key 'move-beginning-of-line)
+(define-key ergoemacs-keymap ergoemacs-split-window-below 'split-window-below)
+(define-key ergoemacs-keymap ergoemacs-split-window-right 'split-window-right)
+(define-key ergoemacs-keymap ergoemacs-delete-window 'delete-window)
+(define-key ergoemacs-keymap ergoemacs-delete-other-windows 'delete-other-windows)
+
+
 
 ;; tabs
 (setq-default indent-tabs-mode nil)
@@ -75,6 +86,7 @@
 (global-set-key (kbd "M-0") 'er/expand-region)
 
 (put 'upcase-region 'disabled nil)
+
 (require 'wrap-region)
 (wrap-region-add-wrapper "/* " " */" "#" '(javascript-mode css-mode js2-mode))
 
