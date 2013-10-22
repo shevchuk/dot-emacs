cedf823ef8a3bbc520a46ab7c2b86eb88552ea62
Mikhail Shevchuk <mikhail.shevchuk@gmail.com>
2013-10-22 17:15:03 +0400

    keymap was introduced

--- init.el
+++ init.el
@@ -53,24 +53,7 @@
 (setq ergoemacs-theme "lvl2")
 (ergoemacs-mode 1)
 
-;; Move to beginning/ending of line
-(defconst ergoemacs-move-beginning-of-line-key	(kbd "M-H"))
-(defconst ergoemacs-move-end-of-line-key	(kbd "M-L"))
-(defconst ergoemacs-delete-window (kbd "M-3"))
-(defconst ergoemacs-delete-other-windows (kbd "M-4"))
-(defconst ergoemacs-split-window-below (kbd "M-8"))
-(defconst ergoemacs-split-window-right (kbd "M-9"))
-
-
-(define-key ergoemacs-keymap ergoemacs-move-end-of-line-key 'move-end-of-line)
-(define-key ergoemacs-keymap ergoemacs-move-beginning-of-line-key 'move-beginning-of-line)
-(define-key ergoemacs-keymap ergoemacs-split-window-below 'split-window-below)
-(define-key ergoemacs-keymap ergoemacs-split-window-right 'split-window-right)
-(define-key ergoemacs-keymap ergoemacs-delete-window 'delete-window)
-(define-key ergoemacs-keymap ergoemacs-delete-other-windows 'delete-other-windows)
-
-
-
+(require 'keymap)
 ;; tabs
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
