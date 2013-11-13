7b0797cd1f5aa892d16cd6fb2b3fac494ddbd622
Mikhail Shevchuk <mikhail.shevchuk@gmail.com>
2013-11-13 16:37:51 +0400

    move region feature was added

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
     org-jira
     projectile
     wrap-region
+    uniquify
 ))
 
 ;; javascript
@@ -88,6 +89,29 @@
                 (lambda () 
                   (interactive)
                   (set-background-color "#aaed52"))) ;; olive
+
+(defun move-region (start end n)
+  "Move the current region up or down by N lines."
+  (interactive "r\np")
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (let ((start (point)))
+      (insert line-text)
+      (setq deactivate-mark nil)
+      (set-mark start))))
+
+(defun move-region-up (start end n)
+  "Move the current line up by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) -1 (- n))))
+
+(defun move-region-down (start end n)
+  "Move the current line down by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) 1 n)))
+
+(global-set-key (kbd "M-<up>") 'move-region-up)
+(global-set-key (kbd "M-<down>") 'move-region-down)
 
 (when (string-equal system-type "darwin")
   (setenv "PATH" 
