329e89329c795f2ead352bceb3f50147bd64edae
Mikhail Shevchuk <mikhail.shevchuk@gmail.com>
2013-12-12 17:31:55 +0400

    indent fix

--- init.el
+++ init.el
@@ -78,11 +78,12 @@
 
 ;; erlang
 (defun start-erlang ()
-	  (setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.6.11/emacs/" load-path))
-	  (setq erlang-root-dir "/usr/local/lib/erlang")
-	  (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
-	  (require 'erlang-start)
-	  (require 'erlang-flymake))
+  (interactive)
+  (setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.6.11/emacs/" load-path))
+  (setq erlang-root-dir "/usr/local/lib/erlang")
+  (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
+  (require 'erlang-start)
+  (require 'erlang-flymake))
 
 ;; tabs
 (setq-default indent-tabs-mode nil)
