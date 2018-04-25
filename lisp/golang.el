b4271696ecf3c0e0c52aa5dee0559468f39f877b
shevchuk <mikhail.shevchuk@gmail.com>
2018-04-25 15:02:47 +0300

    cleaned up the PATH and emacs collaboration deleted reveal.js from the repo

---
+++ lisp/golang.el
@@ -0,0 +1,22 @@
+(require 'company)
+(require 'flycheck)
+(require 'yasnippet)
+(require 'multi-compile)
+(require 'go-eldoc)
+(require 'company-go)
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+(setq-default gofmt-command "goimports")
+(add-hook 'go-mode-hook 'go-eldoc-setup)
+(add-hook 'go-mode-hook (lambda ()
+                            (set (make-local-variable 'company-backends) '(company-go))
+                            (company-mode)))
+(add-hook 'go-mode-hook 'yas-minor-mode)
+(add-hook 'go-mode-hook 'flycheck-mode)
+(setq multi-compile-alist '(
+    (go-mode . (
+("go-build" "go build -v"
+   (locate-dominating-file buffer-file-name ".git"))
+("go-build-and-run" "go build -v && echo 'build finish' && eval ./${PWD##*/}"
+   (multi-compile-locate-file-dir ".git"))))
+    ))
