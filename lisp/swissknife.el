ac1cd391c1bf27b8a0c2495242f2a705bed5b18c
Mike Shevchuk <mikhail.shevchuk@gmail.com>
2020-10-20 11:01:32 +0300

    Add copy path functions

--- lisp/swissknife.el
+++ lisp/swissknife.el
@@ -180,4 +180,16 @@
 (keyfreq-autosave-mode 1)
 ;(wrap-region-add-wrapper "/* " " */" "#" '(javascript-mode css-mode js2-mode))
 
+(defun copy-current-projectpath-to-clipboard ()
+  "Show the full path file name in the minibuffer and copy it to clipboard"
+  (interactive)
+  (kill-new (replace-regexp-in-string (projectile-project-root) "" (buffer-file-name)))
+  (message (replace-regexp-in-string (projectile-project-root) "" (buffer-file-name))))
+
+(defun copy-current-fullpath-to-clipboard ()
+  "Show the full path file name in the minibuffer and copy it to clipboard"
+  (interactive)
+  (kill-new (buffer-file-name))
+  (message (buffer-file-name)))
+
 (provide 'swissknife)
