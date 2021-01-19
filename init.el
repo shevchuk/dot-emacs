73016a0dc8e42da99ed09974d3ebfe183ac6e435
Mike Shevchuk <mikhail.shevchuk@gmail.com>
2021-01-19 13:31:18 +0200

    Add missing packages

--- init.el
+++ init.el
@@ -26,11 +26,15 @@
 (use-package
   typescript-mode :ensure t)
 
+(use-package
+  twig-mode :ensure t)
+
 (use-package lsp-mode
   :ensure t
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (typescript-mode . lsp)
          (js-mode . lsp)
+         (javascript-mode . lsp)
          (php-mode . lsp)
          ;(python-mode . lsp)
          ;; if you want which-key integration
@@ -93,10 +97,15 @@
   :config
   (which-key-mode))
 (use-package smex :ensure t)
+
 (use-package ytdl
   :ensure t
   :init
   (setq ytdl-music-folder (expand-file-name "~/Music")))
+
+(use-package
+  restclient :ensure t)
+
 (use-package dashboard :ensure t)
 (use-package ctrlf :ensure t)
 (use-package prodigy :ensure t)
@@ -129,7 +138,10 @@
 (use-package gherkin-mode :ensure t)
                                         ;multi-eshell
                                         ;vue-mode
-(use-package diff-hl :ensure t)
+(use-package diff-hl
+  :ensure t
+  :hook ((prog-mode . diff-hl-mode)))
+
 (use-package ws-butler :ensure t)
 (use-package expand-region :ensure t)
 (use-package flow-js2-mode :ensure t)
@@ -265,11 +277,12 @@
  '(custom-safe-themes
    '("4ed20d30a768c1a9032cf63aa8980723c21589c68838cd972f79e7ca2a414b9d" "7ea491e912d419e6d4be9a339876293fff5c8d13f6e84e9f75388063b5f794d6" default))
  '(package-selected-packages
-   '(flow-js2-mode js2-mode gherkin-mode puml magit-todos ido-completing-read+ helm-flx yasnippet-snippets yasnippet flx-ido ytdl purescript-mode php-mode lsp-python-ms company company-mode company-capf modus-operandi-theme dap-firefox use-package))
+   '(diff-hl-mode twig-mode rainbow-delimiters rainbow-delimeters diredp dired+ dired-plus restclient flow-js2-mode js2-mode gherkin-mode puml magit-todos ido-completing-read+ helm-flx yasnippet-snippets yasnippet flx-ido ytdl purescript-mode php-mode lsp-python-ms company company-mode company-capf modus-operandi-theme dap-firefox use-package))
  '(plantuml-default-exec-mode 'jar)
  '(plantuml-jar-path "/home/mico/scripts/puml/plantuml.jar")
  '(projectile-globally-ignored-directories
-   '(".idea" ".vscode" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" ".ccls-cache" ".clangd" "ts_build" "node_modules")))
+   '(".idea" ".vscode" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" ".ccls-cache" ".clangd" "ts_build" "node_modules" "vendor"))
+ '(projectile-globally-ignored-file-suffixes '(".map")))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
