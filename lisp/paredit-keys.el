f1ea458398972492ce6609a6716f59b04f39aed5
Mike Shevchuk <mike@shevchuk.org>
2025-03-20 11:18:38 +0200

    add paredit keys

---
+++ lisp/paredit-keys.el
@@ -0,0 +1,50 @@
+;; paredit-open-round (
+;; paredit-close-round )
+;; paredit-close-round-and-newline M-)
+
+;; paredit-open-square [
+;; paredit-close-square ]
+
+;; paredit-doublequote "
+
+;; paredit-forward-delete C-d
+;; paredit-forward-kill-word M-d
+
+;; paredit-forward C-M-f
+;; paredit-backward C-M-b
+
+
+;; paredit-wrap-around M-(
+
+;; * paredit-kill C-i
+
+;; * paredit-splice-sexp C-s
+
+;;   paredit-forward-slurp-sexp C-), C-<right>
+
+;;   paredit-forward-barf-sexp C-}, C-<left>
+
+;;   paredit-backward-slurp-sexp C-(, C-M-<left>, ESC C-<left>
+
+;;   paredit-backward-barf-sexp C-{, C-M-<right>, ESC C-<right>
+
+;; *  paredit-splice-sexp-killing-backward C->
+;; *  paredit-splice-sexp-killing-forward C-<
+
+;;   paredit-raise-sexp C-c r
+;;   paredit-split-sexp C-c s
+;;   paredit-join-sexp C-c j
+
+
+;; * - custom
+
+;; to be defined:
+
+
+;;   paredit-retcentre-on-sexp C-c C-M-l
+;;   paredit-retindent-defun M-q
+
+
+
+
+(provide 'paredit-keys)
