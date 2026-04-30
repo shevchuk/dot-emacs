de982085aa2ceb49bee8c19794a2fe8150c8a178
Mike Shevchuk <mike@shevchuk.org>
2026-04-30 16:59:30 +0300

    use colorful-mode (preview color in buffer) plus safe prefs

--- init.el
+++ init.el
@@ -28,6 +28,21 @@
   typescript-mode :ensure t)
 
 (use-package
+  colorful-mode :ensure t
+  :custom
+  (colorful-use-prefix t)
+  (colorful-only-strings 'only-prog)
+  (css-fontify-colors nil)
+  :config
+  (global-colorful-mode t)
+  (add-to-list 'global-colorful-modes 'helpful-mode))
+
+(add-hook 'conf-mode-hook
+          (lambda ()
+            (colorful-mode 1)))  ;; Ensure colorful-mode is applied after conf-mode
+
+
+(use-package
   twig-mode :ensure t)
 
 ;; straight bootstrap
@@ -92,7 +107,7 @@
 
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/number-convert"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/themes"))
+;(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/themes"))
 
 ;;; first run will install these
 
@@ -307,7 +322,7 @@
 
 ;(use-package modus-operandi-theme :ensure t)
 ; see ui-setup.el that turns the theme on
-(use-package solo-jazz-theme :ensure t)
+;(use-package solo-jazz-theme :ensure t)
 
 (use-package powerline :ensure t)
 (use-package airline-themes :ensure t)
@@ -356,29 +371,108 @@
  ;; If there is more than one, they won't work right.
  '(browse-url-browser-function 'eww-browse-url)
  '(cider-boot-parameters "dev")
+ '(custom-enabled-themes '(ef-spring))
  '(custom-safe-themes
-   '("180821795742c64e155841e8f9351b9d818e865d79dfc72f1aeab16b8724c9e4" "d8bcb88ef0a3259a38d6deba78e569c0750ebfede82ad3e6da16573419fef48c" "2d42de16fa73afacd149338fc6f7a7b3e9fc9b934678ff209f649ea0f24086aa" "d425e2488aee5a2f71abc4a49b1854423dd62575a3529f28aed55eb32190992c" "cd322dc37af17c4e122c99c93fe1a423dd1407797fe51d2278fc25c60a46be45" "9b88b8c64dc30188514f19d1be732ee71cc905b04b0c2c7eb1194528fcebbea4" "d6b05f096d6504aaa0018481dae6fcc9edad1c0658b3f584ea4958edc91714cf" "28cf1f7cc54ab4ee1ba4a4644046bd661941be92ef8327af56909f340cb9d3d5" "7de0917b4064219c0580397495d47b89a5f93d76724504d0ea8d2eea83542167" "e6cdd07a8475458edf8dfd8b5cb3d81c6bb9aa0cb8535322c21d2e242dd044ed" "8672c645e375c0f89575992c230f3f5ab5854b433c238ed871b59a88c8f1c602" "0141fea4ffa9c1fa02834b8681bdddac1b5e7e2bfce6b3c77c5cf0fc604ee8e3" "390af3f30c5d5b76739983cf8b85c8bbd04a46ac1488b11d150dec565ba82f55" "f8abe1fe632952b0fd198907f817accef48bd313fc6ec7ba2470953fcb483a87" "ea066684e9ace1e618719fab683b24a0fbcd3de82692190b1fe54e6b1b2a29bc" "8c01cb4cf9ee298d30a0456b1e90c575d8f5a047e35a5380a5f955c59ed17d2f" "231d11c79b7fb804ef2644580e15a544158d97854af407becfe4f4f78c3de402" "081d5c0ac32e611611bb90fbeaffc7f8d21fe5694180b68cc5243d554d1c3941" "5912c255e7e46432d6c1c057a2124cce807ad4b901a99bc43e838db0754dff91" "d2a14f85e1ba5b28433b96e4628444a4c6a2334368ddc2d568b06eb631492681" "338a3f155def6b84c2126d6ba8c453b2e5eea37b2195dd0465725849ceeeddfe" "8e57da9e594e7eb3a67952a58098e300b2a3be8c7b24bdf5f5b770f0746f7fb5" "a8021746a98a8147069ce4f31e14368c4e7dcf334fe1adff1408b4590d15fb4d" "0e55884b39a023ac216d125b20457d941288de3fcb1358938c37a93b2c394d4d" "8a19e3650988e71cf24b574ee1349c29a1528ed4be506cedc6f15a1ecc9d2355" "88c9a837824eab58891c94728fe0f45623d71eb618dcd0a7183166f17253b1b1" "8cc5fbb8e8dcf9d30f565b87fbe94f5d5c460ddebbf0bdc15e23ad5222ea2578" "27b3336b6115451a340275d842de6e8b1c49ce0bba45210ed640902240f8961d" "2b474647799a29add7e3cf34cd9f8178e872f6a4f354f58f4e717d93a53a4eed" "47f3e55e4e3d570d5513d4cc58047dc059bd6ab3d135c796c4ccbfb77d4eb88b" "ba9c91bc43996f2fa710e4b5145d9de231150103e142acdcf24adcaaf0db7a17" "0edb121fdd0d3b18d527f64d3e2b57725acb152187eea9826d921736bd6e409e" "feb8e98a8a99d78c837ce35e976ebcc97abbd8806507e8970d934bb7694aa6b3" "d6da24347c813d1635a217d396cf1e3be26484fd4d05be153f3bd2b293d2a0b5" "4ed20d30a768c1a9032cf63aa8980723c21589c68838cd972f79e7ca2a414b9d" "7ea491e912d419e6d4be9a339876293fff5c8d13f6e84e9f75388063b5f794d6" default))
+   '("f0ddff48ec78dec9ac9ea3236e7208ef58a86d445207a674523c0eba8e9743b0"
+     "02c9a1ffd2580abac661750036e79d598176458c7af6dc46ebf0aec422052d03"
+     "ac893acecb0f1cf2b6ccea5c70ea97516c13c2b80c07f3292c21d6eb0cb45239"
+     "a7472fb4143839f5bcd9dfdf722933d77b006a2b278f8863a7da2b88ad60b093"
+     "ef518e42a90c237c49fe56de3c0e2706af35556d36e93f723aaf7fc3d85e196b"
+     "b3ba955a30f22fe444831d7bc89f6466b23db8ce87530076d1f1c30505a4c23b"
+     "3d9938bbef24ecee9f2632cb25339bf2312d062b398f0dfb99b918f8f11e11b1"
+     "2551f2b4bc12993e9b8560144fb072b785d4cddbef2b6ec880c602839227b8c7"
+     "a1c18db2838b593fba371cb2623abd8f7644a7811ac53c6530eebdf8b9a25a8d"
+     "f544e09cb6c2a1047a751bcf5623ad70eb45981847b1c673263ce7de4858ae6b"
+     "46dd3b30f3bb26b198eae5cc3d1e4dd79bc0e62af8f3431ae1785ddc4be47d44"
+     "2b8dff32b9018d88e24044eb60d8f3829bd6bbeab754e70799b78593af1c3aba"
+     "8f0a91a1df558a0f6891b9d31bb012ffd2c620caa1a409656a77b96e0ec886a9"
+     "f95e22498a1a22af94542c796f0c330312181cacdcd403ab32e9fe074051c5da"
+     "83e4bedd68c4696410af190367e47b4fcce6ae17be41dcef31beca674e32e7f9"
+     "740813d6cad6d6e5ce610a4b67be4a575a3d22af9fc2e494b6f4c324c9e5a7e2"
+     "f6cf3753918736a982bd5741ef9cd29aa70a09a07830a37796ed3ab7f5da2c5b"
+     "0bc49a65bbc66fde922d08bd73b3068e7763cc98bf83a74dcbf0c66416e720fc"
+     "10077e988cef1b336665e225ae8f7285c0134b90baa6369ee171274f99aa1dd6"
+     "619beec7b0b2986c3b3517a8ae632167022388b9b397ec5f65af3388304239d4"
+     "6b234feec8db588ad5ec2a9d9d7b935f7a155104b25ccfb94d921c45a2ff7d22"
+     "180821795742c64e155841e8f9351b9d818e865d79dfc72f1aeab16b8724c9e4"
+     "d8bcb88ef0a3259a38d6deba78e569c0750ebfede82ad3e6da16573419fef48c"
+     "2d42de16fa73afacd149338fc6f7a7b3e9fc9b934678ff209f649ea0f24086aa"
+     "d425e2488aee5a2f71abc4a49b1854423dd62575a3529f28aed55eb32190992c"
+     "cd322dc37af17c4e122c99c93fe1a423dd1407797fe51d2278fc25c60a46be45"
+     "9b88b8c64dc30188514f19d1be732ee71cc905b04b0c2c7eb1194528fcebbea4"
+     "d6b05f096d6504aaa0018481dae6fcc9edad1c0658b3f584ea4958edc91714cf"
+     "28cf1f7cc54ab4ee1ba4a4644046bd661941be92ef8327af56909f340cb9d3d5"
+     "7de0917b4064219c0580397495d47b89a5f93d76724504d0ea8d2eea83542167"
+     "e6cdd07a8475458edf8dfd8b5cb3d81c6bb9aa0cb8535322c21d2e242dd044ed"
+     "8672c645e375c0f89575992c230f3f5ab5854b433c238ed871b59a88c8f1c602"
+     "0141fea4ffa9c1fa02834b8681bdddac1b5e7e2bfce6b3c77c5cf0fc604ee8e3"
+     "390af3f30c5d5b76739983cf8b85c8bbd04a46ac1488b11d150dec565ba82f55"
+     "f8abe1fe632952b0fd198907f817accef48bd313fc6ec7ba2470953fcb483a87"
+     "ea066684e9ace1e618719fab683b24a0fbcd3de82692190b1fe54e6b1b2a29bc"
+     "8c01cb4cf9ee298d30a0456b1e90c575d8f5a047e35a5380a5f955c59ed17d2f"
+     "231d11c79b7fb804ef2644580e15a544158d97854af407becfe4f4f78c3de402"
+     "081d5c0ac32e611611bb90fbeaffc7f8d21fe5694180b68cc5243d554d1c3941"
+     "5912c255e7e46432d6c1c057a2124cce807ad4b901a99bc43e838db0754dff91"
+     "d2a14f85e1ba5b28433b96e4628444a4c6a2334368ddc2d568b06eb631492681"
+     "338a3f155def6b84c2126d6ba8c453b2e5eea37b2195dd0465725849ceeeddfe"
+     "8e57da9e594e7eb3a67952a58098e300b2a3be8c7b24bdf5f5b770f0746f7fb5"
+     "a8021746a98a8147069ce4f31e14368c4e7dcf334fe1adff1408b4590d15fb4d"
+     "0e55884b39a023ac216d125b20457d941288de3fcb1358938c37a93b2c394d4d"
+     "8a19e3650988e71cf24b574ee1349c29a1528ed4be506cedc6f15a1ecc9d2355"
+     "88c9a837824eab58891c94728fe0f45623d71eb618dcd0a7183166f17253b1b1"
+     "8cc5fbb8e8dcf9d30f565b87fbe94f5d5c460ddebbf0bdc15e23ad5222ea2578"
+     "27b3336b6115451a340275d842de6e8b1c49ce0bba45210ed640902240f8961d"
+     "2b474647799a29add7e3cf34cd9f8178e872f6a4f354f58f4e717d93a53a4eed"
+     "47f3e55e4e3d570d5513d4cc58047dc059bd6ab3d135c796c4ccbfb77d4eb88b"
+     "ba9c91bc43996f2fa710e4b5145d9de231150103e142acdcf24adcaaf0db7a17"
+     "0edb121fdd0d3b18d527f64d3e2b57725acb152187eea9826d921736bd6e409e"
+     "feb8e98a8a99d78c837ce35e976ebcc97abbd8806507e8970d934bb7694aa6b3"
+     "d6da24347c813d1635a217d396cf1e3be26484fd4d05be153f3bd2b293d2a0b5"
+     "4ed20d30a768c1a9032cf63aa8980723c21589c68838cd972f79e7ca2a414b9d"
+     "7ea491e912d419e6d4be9a339876293fff5c8d13f6e84e9f75388063b5f794d6"
+     default))
  '(dashboard-image-banner-max-width 200)
  '(dashboard-startup-banner "~/.emacs.d/img/xemacs_color.svg")
  '(fancy-splash-image "~/.emacs.d/img/xemacs_color.svg")
+ '(ignored-local-variable-values
+   '((cider-jack-in-params "dev") (cider-preferred-build-tool . "boot")))
  '(linum-relative-current-symbol "")
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(mastodon-active-user "mico")
  '(mastodon-instance-url "https://emacs.ch")
  '(nrepl-sync-request-timeout 100)
  '(package-selected-packages
-   '(ellama quelpa pg dashboard-hackernews paredit sqlite3 clj-refactor linum-relative embark org-modern standard-themes emojify mastodon ef-themes excorporate telega cyberpunk-theme clojure-lsp lsp-clojure undo-tree diredfl org-present diff-hl-mode twig-mode rainbow-delimeters diredp dired+ dired-plus restclient flow-js2-mode js2-mode gherkin-mode puml magit-todos ido-completing-read+ helm-flx yasnippet-snippets yasnippet flx-ido ytdl purescript-mode php-mode lsp-python-ms company company-mode company-capf modus-operandi-theme solo-jazz-theme dap-firefox use-package))
+   '(ellama quelpa pg dashboard-hackernews paredit sqlite3 clj-refactor
+            linum-relative embark org-modern standard-themes emojify
+            mastodon ef-themes excorporate telega cyberpunk-theme
+            clojure-lsp lsp-clojure undo-tree diredfl org-present
+            diff-hl-mode twig-mode rainbow-delimeters diredp dired+
+            dired-plus restclient flow-js2-mode js2-mode gherkin-mode
+            puml magit-todos ido-completing-read+ helm-flx
+            yasnippet-snippets yasnippet flx-ido ytdl purescript-mode
+            php-mode lsp-python-ms company company-mode company-capf
+            modus-operandi-theme solo-jazz-theme dap-firefox
+            use-package))
  '(plantuml-default-exec-mode 'jar)
  '(plantuml-jar-path "/home/mico/scripts/puml/plantuml.jar")
  '(projectile-globally-ignored-directories
-   '(".idea" ".vscode" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" ".ccls-cache" ".clangd" "ts_build" "*node_modules" "vendor" "storybook-static" "./var/cache" "*build/static/js" "./backend/vendor" "./service/build" "./service/coverage" ".clj-kondo" "./webapp/out" "./webapp/target"))
+   '(".idea" ".vscode" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout"
+     "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work"
+     ".ccls-cache" ".clangd" "ts_build" "*node_modules" "vendor"
+     "storybook-static" "./var/cache" "*build/static/js"
+     "./backend/vendor" "./service/build" "./service/coverage"
+     ".clj-kondo" "./webapp/out" "./webapp/target"))
  '(projectile-globally-ignored-file-suffixes '(".map" ".log"))
+ '(safe-local-variable-directories '("/home/mico/carify/"))
  '(safe-local-variable-values
-   '((cider-ns-refresh-after-fn . "integrant.repl/resume")
+   '((cider-clojure-cli-global-options . "-A:dev:test")
+     (cider-ns-refresh-after-fn . "integrant.repl/resume")
      (cider-ns-refresh-before-fn . "integrant.repl/suspend")
      (eval progn
-           (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
-           (add-to-list 'cider-jack-in-nrepl-middlewares "shadow.cljs.devtools.server.nrepl/middleware"))))
+           (make-variable-buffer-local
+            'cider-jack-in-nrepl-middlewares)
+           (add-to-list 'cider-jack-in-nrepl-middlewares
+                        "shadow.cljs.devtools.server.nrepl/middleware"))))
  '(undo-tree-auto-save-history t))
 
 (custom-set-faces
